c8ab1f0a20d809d6a8f20def3d0e1973828fe017
harsh <hanzo4679@gmail.com>
2025-09-13 07:41:34 +0000

    cleaup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 
 import { useRouter } from "next/navigation";
@@ -6,6 +7,7 @@ export default function Home() {
   const router = useRouter();
   if (!document.cookie.includes("refreshToken")) {
     router.push("/auth");
+    return;
   }
     
   return (

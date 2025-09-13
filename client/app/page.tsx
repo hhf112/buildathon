d730eb1c940667aa5c7a0d6728510f755d7af7df
harsh <hanzo4679@gmail.com>
2025-09-13 07:48:42 +0000

    repair landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,13 +2,16 @@
 import Image from "next/image";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";  
 
 export default function Home() {
   const router = useRouter();
-  if (!document.cookie.includes("refreshToken")) {
-    router.push("/auth");
-    return;
-  }
+
+  useEffect(() => {
+    if (!document.cookie.includes("refreshToken")) {
+      router.push("/auth");
+    }
+  }, [router]);
     
   return (
     <div className="flex flex-col items-center justify-center h-screen">

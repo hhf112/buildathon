da6cafced759778bc196a37cc5c9fc2cfe37a9bb
harsh <hanzo4679@gmail.com>
2025-09-14 10:10:45 +0000

    dwd

--- client/app/admin/dashboard/SearchClients.tsx
+++ client/app/admin/dashboard/SearchClients.tsx
@@ -11,13 +11,13 @@ import {
 } from "@/components/ui/command"
 import { Warnes } from "next/font/google"
 
-export function SearchClients({elements} : {elements: string[]}) {
+export function SearchClients({ elements }: { elements: string[] }) {
   return <Command>
     <CommandInput placeholder="Type a command or search..." />
     <CommandList>
       <CommandEmpty>No results found.</CommandEmpty>
       <CommandGroup heading="Suggestions">
-      {elements.map(element =>  <CommandItem>{element}</CommandItem>)}
+        {elements.map((element, index) => <CommandItem key={index}>{element}</CommandItem>)}
       </CommandGroup>
     </CommandList>
   </Command>
